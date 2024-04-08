95975e330766bb894146a78df9e99f2a72c8e794
hojay <jeahonglee@gmail.com>
2024-04-08 21:02:49 +0900

    Post create form setup

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import PostCreateForm from '@/posts/post-create-form';
+
 interface TopicShowPageProps {
   params: {
     slug: string;
@@ -12,7 +14,9 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
       </div>
-      <div></div>
+      <div>
+        <PostCreateForm />
+      </div>
     </div>
   );
 }
